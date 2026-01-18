a21b577be00f7628dc7c7b119fdee61088fd7cdb
NULL <129067071+QUWR@users.noreply.github.com>
2026-01-18 16:58:48 +0900

    Initial commit

---
+++ src/main/java/org/example/ddd_study/DddStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.ddd_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ddd_study/DddStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ddd_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
